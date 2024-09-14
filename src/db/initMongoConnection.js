525381f8b5dd5737954be8df99c6e8c5e5239ac3
Denys Polishchuk <polischyk0000@gmail.com>
2024-09-14 19:44:04 +0300

    create app

---
+++ src/db/initMongoConnection.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+import { env } from '../utils/env.js';
+
+export const initMongoConnection = async () => {
+  try {
+    const user = env('MONGODB_USER');
+    const pwd = env('MONGODB_PASSWORD');
+    const url = env('MONGODB_URL');
+    const db = env('MONGODB_DB');
+    console.log(user + pwd + url + db);
+
+    await mongoose.connect(
+      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`,
+    );
+  } catch (e) {
+    console.log('Error while setting up mongo connection', e);
+    throw e;
+  }
+};
