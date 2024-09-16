d49a4d17349da6794b295fc3cbfd461fee5b7c4f
Denys Polishchuk <polischyk0000@gmail.com>
2024-09-16 16:55:55 +0300

    setting start file structure

--- src/server.js
+++ src/server.js
@@ -2,12 +2,12 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 
-import { getAllContacts, getContactById } from './services/contacts.js';
-
 import {
   notFoundMiddleware,
   errorHandlerMiddleWare,
 } from './middlewares/index.js';
+
+import contactsRouter from './routers/contacts.js';
 
 import { env } from './utils/env.js';
 
@@ -28,40 +28,13 @@ export const setupServer = () => {
     }),
   );
 
-  app.get('/', async (req, res) => {
+  app.get('/', (req, res) => {
     res.status(200).json({
       message: 'Hello Friend!',
     });
   });
 
-  app.get('/contacts', async (req, res) => {
-    const contacts = await getAllContacts();
-
-    res.json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data: contacts,
-    });
-  });
-
-  app.get('/contacts/:contactId', async (req, res) => {
-    const { contactId } = req.params;
-    const contact = await getContactById(contactId);
-
-    if (!contact) {
-      res.json({
-        status: 404,
-        message: 'Contact not found',
-      });
-      return;
-    }
-
-    res.json({
-      status: 200,
-      message: `Successfully found contact with id ${contactId}!`,
-      data: contact,
-    });
-  });
+  app.use(contactsRouter);
 
   app.use(notFoundMiddleware);
 
