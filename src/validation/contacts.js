07edbd7322a6e9853e42cdf556e56f71737a4675
Denys Polishchuk <polischyk0000@gmail.com>
2024-09-23 14:34:13 +0300

    create Pagination

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -1,7 +1,7 @@
 import Joi from 'joi';
 
 export const createContactSchema = Joi.object({
-  name: Joi.string().min(3).max(20).required().message({
+  name: Joi.string().min(3).max(20).required().messages({
     'string.base': 'Username should be a string',
     'string.min': 'Username should have at least {#limit} characters',
     'string.max': 'Username should have at most {#limit} characters',
@@ -10,23 +10,22 @@ export const createContactSchema = Joi.object({
   phoneNumber: Joi.string()
     .pattern(/^[0-9]{9,15}$/)
     .required()
-    .message({
+    .messages({
       'string.pattern.base': 'PhoneNumber should be between 9 and 15 digits',
       'any.required': 'PhoneNumber is required',
     }),
-  email: Joi.string().email().message({
+  email: Joi.string().email().messages({
     'string.email': 'Email should be in a valid format (user@gmail.com)',
   }),
-  isFavorite: Joi.boolean().default(false).message({
+  isFavorite: Joi.boolean().default(false).messages({
     'boolean.base': 'Favorite should be a boolean',
   }),
   contactType: Joi.string()
     .valid('work', 'home', 'personal')
     .default('personal')
     .required()
-    .message({
+    .messages({
       'any.only': 'ContactType should be one of [work, home, personal]',
-      'any.required': 'ContactType is required',
     }),
 });
 
