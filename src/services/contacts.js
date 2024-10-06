56371a8ab1aee03737196d47db7ba2289a75b1eb
Denys Polishchuk <polischyk0000@gmail.com>
2024-10-06 14:24:20 +0300

    get contacts whos owner user

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -9,10 +9,13 @@ export const getAllContacts = async ({
   sortOrder = SORT_ORDER.ASC,
   sortBy = '_id',
   filter = {},
+  userId,
 }) => {
   const limit = perPage;
   const skip = (page - 1) * perPage;
   const contactsQuery = ContactsCollection.find();
+
+  contactsQuery.where('userId').equals(userId);
 
   if (filter.isFavorite) {
     contactsQuery.where('isFavorite').equals(filter.isFavorite);
